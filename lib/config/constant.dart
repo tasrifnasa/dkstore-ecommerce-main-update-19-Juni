33ccd07bc22da61f1977d6c80434c87dd11a3ef9
Tasrif Nasa <tasrifnasa@gmail.com>
2026-06-20 09:27:53 +0800

    New Updates

--- lib/config/constant.dart
+++ lib/config/constant.dart
@@ -2,7 +2,7 @@ class AppConstant {
   static String baseUrl = 'https://panel.dkstore.id/api/';
   static String appName = 'Dkstore';
   static String packageName = 'com.dkstore.id';
-  static String androidMapKey = 'AIzaSyDNwwQsWZOcVGq66zgsG_GQed_LepnwtaM';
+  static String androidMapKey = 'AIz';
   static String iosMapKey = 'YOUR_IOS_MAP_KEY';
-  static String serverClientId = '72928386898-1qgms8ltnrrhkq54j7s5fd34et142tut.apps.googleusercontent.com';
+  static String serverClientId = '729283';
 }
